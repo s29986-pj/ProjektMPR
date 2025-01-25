a5c6a415fb678d6c353573d2512ec58224856c72
Przemysław Kosiedowski <s29986@pjwstk.edu.pl>
2025-01-25 13:09:14 +0100

    first commit

---
+++ s29986Bank/src/test/java/com/example/s29986Bank/S29986BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s29986Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S29986BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
